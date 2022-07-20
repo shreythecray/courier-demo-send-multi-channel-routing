d705123eecb4ecca44353595f40d3f308119b145
shreythecray <shrgupta@ucdavis.edu>
2022-07-19 23:44:26 -0700

    added three different functions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { CourierClient } = require("@trycourier/courier");
 const courier = CourierClient({ authorizationToken: process.env.API_KEY });
 
-async function send() {
+async function send_one_channel() {
     const { requestId } = await courier.send({
         message: {
           to: {
@@ -15,11 +15,11 @@ async function send() {
             body: "Thanks for signing up, {{name}}",
           },
           data: {
-            name: "@shreythecray",
+            name: "Peter Parker",
           },
           routing: {
-            method: "all",
-            channels: ["email", "sms"],
+            method: "single",
+            channels: ["email"],
           },
         },
       });
@@ -27,4 +27,52 @@ async function send() {
       console.log(requestId)
 }
 
-send()
+async function send_any_channel() {
+  const { requestId } = await courier.send({
+      message: {
+        to: {
+          email: process.env.EMAIL,
+          phone_number: process.env.PHONE
+        },
+        content: {
+          title: "Welcome!",
+          body: "Thanks for signing up, {{name}}",
+        },
+        data: {
+          name: "Peter Parker",
+        },
+        routing: {
+          method: "single",
+          channels: ["email", "sms"],
+        },
+      },
+    });
+    
+    console.log(requestId)
+}
+
+async function send_multi_channel() {
+  const { requestId } = await courier.send({
+      message: {
+        to: {
+          email: process.env.EMAIL,
+          phone_number: process.env.PHONE
+        },
+        content: {
+          title: "Welcome!",
+          body: "Thanks for signing up, {{name}}",
+        },
+        data: {
+          name: "Peter Parker",
+        },
+        routing: {
+          method: "all",
+          channels: ["email", "sms"],
+        },
+      },
+    });
+    
+    console.log(requestId)
+}
+
+send_one_channel()
